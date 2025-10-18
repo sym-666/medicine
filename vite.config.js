7941f7ec878f6b4710f476283bc4fc4e84731240
shiyiminig <2183444689@qq.com>
2025-10-18 20:42:09 +0800

    部署到github上

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:5173', // 后端 API 服务的地址
         changeOrigin: true, // 是否修改请求源
-        PathRewrite: { '^/api': '' }, // 重写路径
+        pathRewrite: { '^/api': '' }, // 重写路径（修正拼写）
       },
     },
   },
